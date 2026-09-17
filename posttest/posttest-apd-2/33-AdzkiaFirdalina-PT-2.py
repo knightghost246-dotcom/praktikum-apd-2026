bcddff69579802d6d76239c4491d87afb9d98335
root <knightghost246@gmail.com>
2026-09-17 23:32:49 +0800

    tambah posttest 2 - bagasi kabin - Adzkia Firdalina

---
+++ posttest/posttest-apd-2/33-AdzkiaFirdalina-PT-2.py
@@ -0,0 +1,29 @@
+# Posttest 2
+# Nama: AdzkiaFirdalina
+# NIM : 33
+
+Bagasi = [12, 18, 7, 15, 20, 10]
+
+Total_Berat_Akhir = (Bagasi[0] + Bagasi[1] + Bagasi[2] +
+                     Bagasi[3] + Bagasi[4] + Bagasi[5])
+
+Total_Bayar = Total_Berat_Akhir * 0.05
+
+Rata_Rata = Total_Bayar / len(Bagasi)
+
+Nim = 33 
+
+Bolean = Nim < Rata_Rata
+
+Total_Berat_Akhir_Gram = Total_Berat_Akhir * 1000
+
+Data_Tengah = Bagasi[2:5]  
+
+print("Bagasi =", Bagasi)
+print("Total_Berat_Akhir =", Total_Berat_Akhir)
+print("Total_Bayar =", Total_Bayar)
+print("Rata_Rata =", Rata_Rata)
+print("Nim =", Nim)
+print("Bolean =", Bolean)
+print("Total_Berat_Akhir_Gram =", Total_Berat_Akhir_Gram)
+print("Data_Tengah =", Data_Tengah)
